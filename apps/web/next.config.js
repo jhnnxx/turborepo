e606977d9d6c5918edf91310e95754ae90484b9b
이성준 leeseongjun <jvden@commerceon.co.kr>
2025-04-02 18:43:32 +0900

    first commit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/docs/:path*",
+				destination: "http://localhost:3001/:path*",
+				permanent: false,
+			},
+		];
+	},
+};
 
 export default nextConfig;
